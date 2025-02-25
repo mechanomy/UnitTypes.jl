3e3228a97d08a9ae875f629412984d535dc71589
Ben <ben@benconrad.net>
2025-02-25 06:37:26 -0600

    switching branches

--- docs/makeUnitTypesTree.jl
+++ docs/makeUnitTypesTree.jl
@@ -1,6 +1,13 @@
+"""
+Prints the type tree to the file
+"""
 
 using UnitTypes
 import InteractiveUtils
 import AbstractTrees
 AbstractTrees.children(d::DataType) = InteractiveUtils.subtypes(d)
-AbstractTrees.print_tree(AbstractMeasure)
+# AbstractTrees.print_tree(AbstractMeasure) #print to repl
+
+open("unitTypesTree.txt", "w") do fid
+  AbstractTrees.print_tree(fid, AbstractMeasure)
+end
